061f9c766cc6b619b2d357f6423d0a3bf373525e
Eduardo Sarango <efas_24_69@hotmail.com>
2018-05-31 11:44:44 -0500

    cambios

--- Semana9/src/semana9/Arreglo.java
+++ Semana9/src/semana9/Arreglo.java
@@ -15,10 +15,12 @@ public class Arreglo {
          int multiplica =1;
          
          for(int contador=0 ; contador < arreglo.length  ; contador ++){
+             //para determinar numero par en mi lista.
              if (arreglo[contador] % 2 == 0){
                  for (int contador1=1 ; contador1 <=12  ; contador1 ++){
-                  
+                     //operamos los valires de la lista.
                      multiplica = arreglo[contador] * contador1;
+                     //imprimimos las tablas.
                     System.out.printf("%d * %d = %d\n",arreglo[contador] ,contador1 , multiplica );
                  }
                  System.out.println("------------");
--- Semana9/src/semana9/Promedionotas.java
+++ Semana9/src/semana9/Promedionotas.java
@@ -11,16 +11,18 @@ package semana9;
  */
 public class Promedionotas {
     public static void main(String[] args) {
+        // Declaramos los arreglos a usar.
         double[]calificaciones_progr= {8, 19, 15, 16 ,17 ,10};
         double[]calificaciones_bd= {10, 12, 13, 20 ,17 ,20};
         double[] promedios= new double [6];
-        
+        //Recorremos arreglos para la suma .
         for (int contador= 0; contador< calificaciones_bd.length ; contador ++ ){
           double suma = calificaciones_progr[contador] + calificaciones_bd[contador];  
           double promedio = suma/2;
-            //System.out.printf("Estudiante %d\n%f\n%f",contador ,);
+         //guardamos resultados en la  lista promedios
           promedios[contador] = promedio;
         }
+        //imprimimos valores.
         for (int contador1= 0 ; contador1 < calificaciones_bd.length ; contador1 ++ ){
             System.out.printf("Estudiante %d %-15f%-15f%-15f\n",contador1+1, calificaciones_progr[contador1] , calificaciones_bd[contador1], promedios[contador1]);
         }
