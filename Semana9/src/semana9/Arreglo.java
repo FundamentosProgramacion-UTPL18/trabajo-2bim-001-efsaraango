c616bc797f6c89cd8729531482e1b4a2e1a8445b
Eduardo Sarango <efas_24_69@hotmail.com>
2018-05-31 11:49:39 -0500

    cambios

--- Semana9/src/semana9/Arreglo.java
+++ Semana9/src/semana9/Arreglo.java
@@ -10,27 +10,24 @@ package semana9;
  * @author SARANGO
  */
 public class Arreglo {
-     public static void main(String[] args) {
-         int[] arreglo = {1 ,2 ,3 ,4 ,5 ,6 ,7 };
-         int multiplica =1;
-         
-         for(int contador=0 ; contador < arreglo.length  ; contador ++){
-             //para determinar numero par en mi lista.
-             if (arreglo[contador] % 2 == 0){
-                 for (int contador1=1 ; contador1 <=12  ; contador1 ++){
-                     //operamos los valires de la lista.
-                     multiplica = arreglo[contador] * contador1;
-                     //imprimimos las tablas.
-                    System.out.printf("%d * %d = %d\n",arreglo[contador] ,contador1 , multiplica );
-                 }
-                 System.out.println("------------");
-             } 
-         
-        
-         
-         
-         
-         }
 
-     }
+    public static void main(String[] args) {
+        int[] arreglo = {1, 2, 3, 4, 5, 6, 7};
+        int multiplica = 1;
+
+        for (int contador = 0; contador < arreglo.length; contador++) {
+            //para determinar numero par en mi lista.
+            if (arreglo[contador] % 2 == 0) {
+                for (int contador1 = 1; contador1 <= 12; contador1++) {
+                    //operamos los variables de la lista.
+                    multiplica = arreglo[contador] * contador1;
+                    //imprimimos las tablas.
+                    System.out.printf("%d * %d = %d\n", arreglo[contador], contador1, multiplica);
+                }
+                System.out.println("------------");
+            }
+
+        }
+
+    }
 }
