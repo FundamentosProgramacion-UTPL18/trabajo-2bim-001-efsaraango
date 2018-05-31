fecfee486945024151f7d465fcedd4ccfe6e4273
Eduardo Sarango <efas_24_69@hotmail.com>
2018-05-31 10:15:56 -0500

    Trabajo listas

---
+++ Semana9/src/semana9/Semana9.java
@@ -0,0 +1,26 @@
+
+package semana9;
+
+/**
+ *
+ * @author SARANGO
+ */
+public class Semana9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int[] c = new int[10];
+        c [0] = 10;
+        
+        for(int contador = 0; contador < c.length ; contador ++){
+            System.out.println( c[contador] );
+        
+        }
+        
+        
+    }
+    
+}
